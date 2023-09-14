cd177eff1aa60d591542feea73c6066065327cdc
Alan Perez <dperez@ibw.com>
2023-09-14 10:26:29 -0600

    update output.tf

--- output.tf
+++ output.tf
@@ -1,13 +1,11 @@
 output "public_ip" {
-  value = aws_instance.windows.public_ip
+  value = aws_eip.windows.public_ip
 }
 output "public_dns" {
-  value = aws_instance.windows.public_dns
+  value = aws_eip.windows.public_dns
 }
-#output "windows_password" {
-#  value = aws_instance.windows.password_data
-#}
 output "password_decrypted" {
-###modify the next line with your file path###
+## change the path of your key
   value=rsadecrypt(aws_instance.windows.password_data, file("~/.ssh/new_id_rsa") )
 }
+
